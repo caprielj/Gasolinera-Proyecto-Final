0121e600d75af9738484a7e50d94489db4068703
Jefrid Estuardo Capriel Perez <133267021+caprielj@users.noreply.github.com>
2024-05-29 00:44:29 -0600

    Add files via upload

--- Proyecto Gasolinera/Proyecto Gasolinera/Abastecimiento.cs
+++ Proyecto Gasolinera/Proyecto Gasolinera/Abastecimiento.cs
@@ -14,22 +14,29 @@ namespace Proyecto_Gasolinera
         private string tipo;
         private double cantidad;
         private double precio;
-
-        public Abastecimiento(Cliente cliente, DateTime fecha, string tipo, double cantidad, double precio)
+        private int bomba;
+        private Abastecimiento siguiente;
+        public Abastecimiento()
         {
-            this.Cliente = cliente;
-            this.fecha = fecha;
-            this.tipo = tipo;
-            this.cantidad = cantidad;
-            this.precio = precio;
+            this.cliente = null;
+            this.fecha = DateTime.Now;
+            this.fecha2 = fecha.ToString("dd/MM/yyyy");
+            this.tipo = "";
+            this.cantidad = 0;
+            this.precio = 0;
+            this.bomba = 0;
+            this.siguiente = null;
         }
 
         internal Cliente Cliente { get => cliente; set => cliente = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
-        
+        public string Fecha2 { get => fecha2; set => fecha2 = value; }
+        public Abastecimiento Siguiente { get => siguiente; set => siguiente = value; }
+
         public string Tipo { get => tipo; set => tipo = value; }
         public double Cantidad { get => cantidad; set => cantidad = value; }
         public double Precio { get => precio; set => precio = value; }
+        public int Bomba { get => bomba; set => bomba = value; }
 
     }
 }
